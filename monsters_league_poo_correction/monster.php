51eb25d256d5fe4cbec2e952399a83e81d142ef0
Splinntz <45148244+Splinntz@users.noreply.github.com>
2019-01-27 19:57:36 +0100

    Add files via upload

---
+++ monsters_league_poo_correction/monster.php
@@ -0,0 +1,45 @@
+<?php
+class Monster{
+    private $name = 'Default';
+    private $strength;
+    private $life;
+    private $type;
+    
+    function __construct($nameP, $strengthP, $lifeP, $typeP){
+        $this->name = $nameP;
+        $this->strength = $strengthP;
+        $this->life = $lifeP;
+        $this->type = $typeP;
+    }
+
+    function setName($value){
+        $this->name = $value;
+    }
+    function getName(){
+        return $this->name;
+    }
+
+    function setStrength($value){
+        $this->strength = $value;
+    }
+    function getStrength(){
+        return $this->strength;
+    }
+
+    function setLife($value){
+        $this->life = $value;
+    }
+    function getLife(){
+        return $this->life;
+    }
+
+    function setType($value){
+        $this->type = $value;
+    }
+    function getType(){
+        return $this->type;
+    }
+
+}
+
+?>
